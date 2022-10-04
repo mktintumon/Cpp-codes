4a412b4ed4de8a7e760a3a084a02920c64bf602d
mktintumon <mktintumon@gmail.com>
2022-10-04 10:49:17 +0530

    added

---
+++ Questions/infixToPostAndPrefix.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <ctype.h>
+using namespace std;
+
+// precedence checker
+int precd(char ch){
+    if(ch == '+' || ch == '-') return 1;
+    else return 2; //  -->(* ans /)
+}
+ 
+int main(){
+    string str;
+    cout << "Enter Infix Expression: ";
+    cin >> str;
+
+    stack<string> prefix;
+    stack<string> postfix;
+    stack<char> optr;
+
+    for(int i=0; i<str.length(); i++){
+        char ch = str[i];
+
+        if(ch == '('){
+            optr.push(ch);
+        }
+        else if((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+            string s = string(1,ch);
+            prefix.push(s);
+            postfix.push(s);
+        }
+        else if(ch == ')'){
+            while(optr.size() > 0 && optr.top() != '('){
+                char op = optr.top();
+                optr.pop();
+                string s = string(1,op);
+
+                //prefix
+                string pre2 = prefix.top();
+                prefix.pop();
+                string pre1 = prefix.top();
+                prefix.pop();
+                prefix.push(s + pre1 + pre2);
+
+                //postfix
+                string post2 = postfix.top();
+                postfix.pop();
+                string post1 = postfix.top();
+                postfix.pop();
+                postfix.push(post1 + post2 + s);
+            }
+            optr.pop();
+        }
+        else if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+            while(optr.size() > 0 && optr.top() != '(' && precd(ch) <= precd(optr.top())){
+                char op = optr.top();
+                optr.pop();
+                string s = string(1,op);
+
+                //prefix
+                string pre2 = prefix.top();
+                prefix.pop();
+                string pre1 = prefix.top();
+                prefix.pop();
+                prefix.push(s + pre1 + pre2);
+
+                //postfix
+                string post2 = postfix.top();
+                postfix.pop();
+                string post1 = postfix.top();
+                postfix.pop();
+                postfix.push(post1 + post2 + s);
+            }
+            optr.push(ch);
+        }
+    }
+
+    while(optr.size() >  0){
+        char op = optr.top();
+        optr.pop();
+        string s = string(1,op);
+
+        //prefix
+        string pre2 = prefix.top();
+        prefix.pop();
+        string pre1 = prefix.top();
+        prefix.pop();
+        prefix.push(s + pre1 + pre2);
+
+        //postfix
+        string post2 = postfix.top();
+        postfix.pop();
+        string post1 = postfix.top();
+        postfix.pop();
+        postfix.push(post1 + post2 + s);
+    }
+
+    cout << endl;
+    cout << "Prefix Expression: " << prefix.top() << endl;
+    cout << "Postfix Expression: " << postfix.top() << endl;
+
+    return 0;
+}
