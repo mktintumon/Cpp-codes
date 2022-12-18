3aac73f1af7734546e066ac45228797c897c1c4f
mktintumon <mktintumon@gmail.com>
2022-12-18 15:52:35 +0530

    added

--- GRAPH/construction.cpp
+++ GRAPH/construction.cpp
@@ -1,27 +1,37 @@
 #include <bits/stdc++.h>
-#include<vector>
 using namespace std;
 
-class Graph{
-    int V;
-    vector<int> *g;
-
+class Edge{
     public:
+        int u,v,wt;
+
+        Edge(int u,int v,int wt){
+            this->u = u;
+            this->v = v;
+            this->wt = wt;
+        }
+};
+
+class Graph{
+    public:
+        int V; // no of vertex
+        vector<Edge> *g;
+
         Graph(int V){
             this->V = V;
-            g = new vector<int>[V];
+            g = new vector<Edge>[V];
         }
 
-        void addEdge(int u , int v){
-            g[u].push_back(v);
-            g[v].push_back(u);
+        void addEdge(int u , int v , int wt){
+            g[u].push_back(Edge(u,v,wt));
+            g[v].push_back(Edge(v,u,wt));
         }
 
         void printGraph(){
             for(int i=0 ; i<V; i++){
                 cout << "Vertex " << i << " -> ";
-                for(int nbr : g[i]){
-                    cout << nbr << "   ";
+                for(Edge nbr : g[i]){
+                    cout << "[" << nbr.u << " - " << nbr.v  << " @ " <<  nbr.wt << "] , " ;
                 }
                 cout << endl;
             }
@@ -30,10 +40,10 @@ class Graph{
 
 int main(){
     Graph g(4);
-    g.addEdge(0,1);
-    g.addEdge(0,2);
-    g.addEdge(2,3);
-    g.addEdge(1,2);
+    g.addEdge(0,1,10);
+    g.addEdge(0,2,20);
+    g.addEdge(2,3,30);
+    g.addEdge(1,2,40);
     g.printGraph();
 
     return 0;
